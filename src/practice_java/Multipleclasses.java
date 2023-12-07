70fa96986d52e8706a766e6a82bde3c455a5c33e
Admin <Admin@DESKTOP-GVLMBEC>
2023-12-07 14:43:47 +0530

    commit msg..

---
+++ src/practice_java/Multipleclasses.java
@@ -0,0 +1,128 @@
+package practice_java;
+
+public class Multipleclasses {
+
+
+
+}
+
+class Human{
+	String name;
+	int age;
+	String gender;
+	
+	void details() {
+		
+	}
+}
+
+class Laptop{
+	String brand;
+	String model;
+	String processor;
+	int ramSize;
+	int price;
+	
+	void work() {
+		
+	}
+}
+ 
+class SportMan{
+	String name;
+	String sport;
+	String team;
+	String position;
+	
+	void play() {
+		
+	}
+}
+
+class Testcase{
+	String name;
+	String description;
+	String status;
+	
+	void scenario() {
+		
+	}
+}
+
+class Cupboard{
+	String type;
+	String shape;
+	String color;
+	double size;
+	double length;
+	double height;
+	int price;
+	
+	void store() {
+		
+	}
+	
+}
+
+class Smartphone{
+	String brand;
+	String model;
+	String color;
+	int price;
+	
+	void call() {
+		
+	}
+}
+
+class Car{
+	String model;
+	String color;
+	int year;
+	int price;
+void drive() {
+	
+}
+}
+class Book{
+	String author;
+	String genre;
+	String title;
+	int pages;
+	int price;
+	
+	void read() {
+		
+	}
+}
+
+class Dog{
+	String breed;
+	String name;
+	String color;
+	int age;
+	
+	void bark() {
+}
+}
+
+class Teacher{
+	String name;
+	String gender;
+	String subject;
+	int experienceyears;
+	int age;
+	
+	void study() {
+	
+}
+}
+ class Student{
+	 String name;
+	 String gender;
+	 int age;
+	 
+	 void study() {
+	 
+ }
+	 }
